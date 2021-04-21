e38c58aa7656e4d1f2d8fc896dbcf972ba7c8031
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-04-20 23:36:32 -0300

    Remove apologies

--- me-desculpa/script.js
+++
@@ -1,7 +0,0 @@
-let chocolatePieces = document.querySelectorAll('.chocolate-piece');
-
-for (let piece of chocolatePieces) {
-  piece.addEventListener('click', (e) => {
-    e.target.classList.toggle('eaten');
-  });
-}
